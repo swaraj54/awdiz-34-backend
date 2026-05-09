989477ac1d03444ae8b4475895432d44e1288d1c
Swaraj Jadhav <70018714+swaraj54@users.noreply.github.com>
2026-05-09 10:52:50 +0530

    Updates for day 11

--- Day-6/controllers/auth.controllers.js
+++ Day-6/controllers/auth.controllers.js
@@ -1,4 +1,5 @@
 import UserModel from "../models/user.schema.js";
+import bcrypt from "bcrypt";
 
 export const Register = async (req, res) => {
   try {
@@ -7,17 +8,26 @@ export const Register = async (req, res) => {
       return res.status(400).json({ message: "All fields are required" });
     }
 
+    const isUserExist = await UserModel.findOne({ email });
+    if (isUserExist) {
+      return res
+        .status(400)
+        .json({ message: "User already exists, Please login." });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    console.log("Hashed Password:", hashedPassword);
     const newUser = new UserModel({
       name: name,
       email: email,
-      password: password,
+      password: hashedPassword,
       role: role,
     });
     console.log("New User:", newUser);
     await newUser.save();
     return res.status(201).json({
       message: "User registered successfully",
-      user: { name, email, password, role },
+      user: newUser,
     });
   } catch (error) {
     return res
@@ -26,10 +36,60 @@ export const Register = async (req, res) => {
   }
 };
 
-export const Login = (req, res) => {
-  res.send("User logged in successfully from controller.");
+export const Login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Email and password are required" });
+    }
+    const user = await UserModel.findOne({ email });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ message: "User not found, Please register or check email." });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    console.log("Is Password Valid:", isPasswordValid);
+    if (!isPasswordValid) {
+      return res
+        .status(400)
+        .json({ message: "Invalid password, Please try again." });
+    }
+
+    // token creation - jwt- jsonwebtoken 
+    // store token into cookie
+
+    return res
+      .status(200)
+      .json({ message: "User logged in successfully", user: user });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Error logging in user", error: error.message });
+  }
 };
 
 export const Logout = (req, res) => {
   res.send("User logged out successfully from controller.");
+};
+
+export const UpdateUserPassword = async (req, res) => {
+  try {
+    const users = await UserModel.find();
+    // for (let i = 0; i < users.length - 2; i++) {
+    //   console.log("Users:", users[i]);
+    //   const hashedPassword = await bcrypt.hash(users[i].password, 10);
+    //   await UserModel.findByIdAndUpdate(users[i]._id, {
+    //     password: hashedPassword,
+    //   });
+    // }
+    res.send(true);
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Error updating user password", error: error.message });
+  }
 };
--- Day-6/routes/auth.routes.js
+++ Day-6/routes/auth.routes.js
@@ -1,9 +1,15 @@
 import { Router } from "express";
-import { Login, Logout, Register } from "../controllers/auth.controllers.js";
+import {
+  Login,
+  Logout,
+  Register,
+  UpdateUserPassword,
+} from "../controllers/auth.controllers.js";
 
 const AuthRouter = Router();
 
 AuthRouter.post("/register", Register);
+AuthRouter.put("/update-user-password", UpdateUserPassword);
 AuthRouter.post("/login", Login);
 AuthRouter.post("/logout", Logout);
 
