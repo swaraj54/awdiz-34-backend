0e98e7e23bc858e0e3a3fd7f6cbe34d532489103
Swaraj Jadhav <70018714+swaraj54@users.noreply.github.com>
2026-05-30 12:54:16 +0530

    Fe deployed url added for cors

--- Day-6/index.js
+++ Day-6/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: ["http://localhost:5173", "http://localhost:5174"],
+  origin: ["http://localhost:5173", "https://awdiz-34-react.vercel.app"],
   credentials: true,
 };
 app.use(cors(corsOptions));
